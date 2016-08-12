e934d44154af96dff5d258f14f5f8b3b32c9ebae
yotamlaufer <yotam@lostmy.name>
2016-08-12 12:17:59 +0100

    - wip

---
+++ lib/output.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+info() {
+  echo "       $*" || true
+}
+
+# format output and send a copy to the log
+output() {
+  local logfile="$1"
+
+  while read LINE;
+  do
+    echo "       $LINE" || true
+    echo "$LINE" >> "$logfile" || true
+  done
+}
+
+header() {
+  echo "" || true
+  echo "-----> $*" || true
+}
+
+error() {
+  echo " !     $*" >&2 || true
+  echo "" || true
+}
